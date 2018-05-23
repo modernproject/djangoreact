74422b6d76b01c04fc71698d5be336e7e38e3a5b
Dylan Stein <djstein@ncsu.edu>
2018-05-22 20:36:36 -0400

    initial folder structure

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
+import replace from "rollup-plugin-replace";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+
+const env = process.env.NODE_ENV
+
+const config = {
+  input: 'src/index.js',
+  external: ['react', 'redux'],
+  output: {
+    format: 'umd',
+    name: 'Jackal',
+    globals: {
+      react: 'React',
+      redux: 'Redux'
+    }
+  },
+  plugins: [
+    babel({
+      exclude: '**/node_modules/**',
+      plugins: ["external-helpers"]
+    }),
+    resolve(),
+    commonjs({
+      include: /node_modules/
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+export default config
