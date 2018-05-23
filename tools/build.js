2291a9389371ec3579f9bc7b416683651cb295c1
Dylan Stein <djstein@ncsu.edu>
2018-05-22 21:16:19 -0400

    first bundle build

---
+++ tools/build.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const execSync = require('child_process').execSync
+const prettyBytes = require('pretty-bytes')
+const gzipSize = require('gzip-size')
+
+const exec = (command, extraEnv) =>
+  execSync(command, {
+    stdio: 'inherit',
+    env: Object.assign({}, process.env, extraEnv)
+  })
+
+console.log('Building CommonJS ...')
+
+exec('babel src -d . --ignore __tests__', {
+  BABEL_ENV: 'cjs'
+})
+
+console.log('\nBuilding ES ...')
+
+exec('babel src -d es --ignore __tests__', {
+  BABEL_ENV: 'es'
+})
+
+console.log('\nBuilding jackal.js ...')
+
+exec('rollup -c -f umd -o umd/index.js', {
+  BABEL_ENV: 'umd',
+  NODE_ENV: 'development'
+})
+
+console.log('\nBuilding jackal.min.js ...')
+
+exec('rollup -c -f umd -o umd/index.min.js', {
+  BABEL_ENV: 'umd',
+  NODE_ENV: 'production'
+})
+
+const size = gzipSize.sync(
+  fs.readFileSync('umd/index.min.js')
+)
+
+console.log('\ngzipped, the UMD build is %s', prettyBytes(size))
